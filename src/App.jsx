852ff59b7c13870b785091ebd8727897e00efe6d
nhwnahid007 <nhwnahid@gmail.com>
2024-03-13 17:20:51 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <p className='text-4xl  bg-gray-500 '>Check</p>
+    </>
+  );
+}
+
+export default App
